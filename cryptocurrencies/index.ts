ddb3341f83414e596a45ab9943689aa974203acb
Zach Richardson <zach@forsta.io>
2021-06-27 11:16:55 -0600

    commented currencies

--- cryptocurrencies/index.ts
+++ cryptocurrencies/index.ts
@@ -1,27 +1,47 @@
+/*-- CORE --*/
 import { fetchAPI, fetchIntradayQuote, fetchQuote } from "../core";
 import FUNCTIONS from "../core/enums";
-import { ICurrencyExchangeRate, ICryptoRating } from "./interfaces";
+import { IRequest } from "../core/interfaces";
 
+/*-- CRYPTOCURRENCY --*/
+import { ICurrencyExchangeRate } from "./interfaces";
+
+
+// CURRENCY_EXCHANGE_RATE
 export const currencyExchangeRate = (params: ICurrencyExchangeRate) =>
-  fetchAPI<ICurrencyExchangeRate>(params);
+  fetchAPI(params);
 
-export const cryptoRating = (params: ICryptoRating) =>
-  fetchAPI<ICryptoRating>(params);
+// CRYPTO_RATING
+export const cryptoRating = (params: IRequest) =>
+  fetchAPI(params);
 
+// CRYPTO_INTRADAY
 export const cryptoIntraday = (params) =>
   fetchIntradayQuote({
     ...params,
     function: FUNCTIONS.cryptoIntraday,
   });
 
+// DIGITAL_CURRENCY_DAILY
 export const digitalCurrencyDaily = (params) =>
-  fetchQuote({ ...params, function: FUNCTIONS.digitalCurrencyDaily });
+  fetchQuote({ 
+    ...params,
+    function: FUNCTIONS.digitalCurrencyDaily
+  });
 
+// DIGITAL_CURRENCY_WEEKLY
 export const digitalCurrencyWeekly = (params) =>
-  fetchQuote({ ...params, function: FUNCTIONS.digitalCurrencyWeekly });
+  fetchQuote({
+    ...params,
+    function: FUNCTIONS.digitalCurrencyWeekly
+  });
 
+// DIGITAL_CURRENCY_MONTHLY
 export const digitalCurrencyMonthly = (params) =>
-  fetchQuote({ ...params, function: FUNCTIONS.digitalCurrencyMonthly });
+  fetchQuote({
+    ...params,
+    function: FUNCTIONS.digitalCurrencyMonthly
+  });
 
 export default {
   currencyExchangeRate,
--- forex/index.ts
+++ forex/index.ts
@@ -1,24 +1,40 @@
+/*-- CORE --*/
 import { fetchAPI, fetchIntradayQuote, fetchQuote } from "../core";
 import FUNCTIONS from "../core/enums";
+
+/*-- FOREX --*/
 import { ICurrencyExchangeRate } from "./interfaces";
 
+// CURRENCY_EXCHANGE_RATE
 export const currencyExchangeRate = (params: ICurrencyExchangeRate) =>
-  fetchAPI<ICurrencyExchangeRate>(params);
+  fetchAPI(params);
 
+// FX_INTRADAY
 export const fxIntraday = (params) =>
   fetchIntradayQuote({
     ...params,
     function: FUNCTIONS.fxIntraday,
   });
 
+// FX_DAILY
 export const fxDaily = (params) =>
-  fetchQuote({ ...params, function: FUNCTIONS.fxDaily });
+  fetchQuote({
+    ...params,
+    function: FUNCTIONS.fxDaily
+  });
 
+// FX_WEEKLY
 export const fxWeekly = (params) =>
-  fetchQuote({ ...params, function: FUNCTIONS.fxWeekly });
+  fetchQuote({
+    ...params,
+    function: FUNCTIONS.fxWeekly
+  });
 
+// FX_MONTHLY
 export const fxMonthly = (params) =>
-  fetchQuote({ ...params, function: FUNCTIONS.fxMonthly });
+  fetchQuote({
+    ...params,
+    function: FUNCTIONS.fxMonthly});
 
 export default {
   currencyExchangeRate,
